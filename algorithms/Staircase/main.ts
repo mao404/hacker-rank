f4da5e9f5977d5f21d82129aa31f3db17cba5992
Luis <luispadron25@gmail.com>
2025-02-04 18:54:19 -0500

    Staircase

--- algorithms/Staircase/main.ts
+++ algorithms/Staircase/main.ts
@@ -1,3 +1,10 @@
 function staircase(n: number): void {
-    //
+  for (let i = 1; i <= n; i++) {
+    // Generate spaces: (n - i) spaces
+    let spaces = " ".repeat(n - i);
+    // Generate hashes: i hashes
+    let hashes = "#".repeat(i);
+    // Print the row
+    console.log(spaces + hashes);
+  }
 }
